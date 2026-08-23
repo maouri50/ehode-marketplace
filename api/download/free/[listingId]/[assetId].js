c1b8ad582d94523489d4be39b5e5853c75298f08
Ehode deployment assistant <deployments@ehode.com>
2026-08-23 15:44:55 +0000

    Add concrete Vercel digital download routes

---
+++ api/download/free/[listingId]/[assetId].js
@@ -0,0 +1,4 @@
+import app from "../../../handler.mjs";
+
+// Concrete dynamic segments are required by Vercel for public free downloads.
+export default app;
---
+++ api/download/paid/[token].js
@@ -0,0 +1,4 @@
+import app from "../../handler.mjs";
+
+// Concrete dynamic segment is required by Vercel for protected receipt downloads.
+export default app;
